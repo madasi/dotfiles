9297bec18310b5f769ed0d9c01c393c98fb758b0
Madasi <madasi@gmail.com>
2023-04-04 12:17:19 -0500

    Moved from NetworkManager to systemd-networkd

--- dot_config/i3/scripts/executable_tray.sh
+++ dot_config/i3/scripts/executable_tray.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-pkill -f nm-applet
+# pkill -f nm-applet
+pkill -f networkd-notify
 pkill -f variety
 pkill -f solaar
 
-# NetworkManager is the most popular way to manage wireless networks on Linux,
-# and nm-applet is a desktop environment-independent system tray GUI for it.
-# nm-applet &
-nm-applet --indicator &
+# I don't use NetworkManger any longer
+# # NetworkManager is the most popular way to manage wireless networks on Linux,
+# # and nm-applet is a desktop environment-independent system tray GUI for it.
+# # nm-applet &
+# nm-applet --indicator &
+networkd-notify &
 
 # Backgrounds please
 variety &
